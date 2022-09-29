a39d336e0120341105d9eb8959897eb9605439e5
rodrigo videira <rodrigodonizetevideira@gmail.com>
2022-09-29 14:16:18 -0300

    alteracoes

---
+++ AppGuia/AppGuia/Models/Componente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGuia.Models
+{
+    public class Componente
+    {
+        public string Name { get; set; } 
+
+        public string CargaHoraria { get; set; }
+
+        public string AtribuicoesResponsabilidades { get; set; }
+
+        public string ValoresAtitudes { get; set; }
+
+        public string Competencias { get; set; }
+
+        public string Habilidades { get; set; }
+
+        public string BasesTecnologicas { get; set; }
+
+    }
+}
